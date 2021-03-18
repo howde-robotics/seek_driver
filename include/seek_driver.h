ddc955d3ed8f1753489af49d2f4ea9a2fc0e1b3b
Dan Bronstein <danbronzy@gmail.com>
2021-03-17 23:14:20 -0400

    Functional, pushes only display messages

--- include/seek_driver.h
+++ include/seek_driver.h
@@ -2,48 +2,94 @@
 // This file is subject to the terms and conditions defined in the file 'LICENSE',
 // which is part of this source code package
 
-// always add include guard for all .h files
 #pragma once
 
 // std libraries
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <memory>
 
 // ROS libraries
 #include <ros/ros.h>
 #include <std_msgs/Int32.h>
 #include <std_msgs/Float64.h>
-#include <std_msgs/String.h>
+#include <std_msgs/String.h>   
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
 
 // Include other header files
-#include "seekware.h"
+#include <seekware.h>
 #include <opencv2/opencv.hpp>
 
 /**
- * @brief Explain briefly what this class does, e.g. it takes in this input, does this process and output this
- *
- * This part is now the detailed explanation of the class, you can write entire length essays (highly encouraged btw)
- * Explain what algo you use, any important details
- *
- * @see PythonNode
- * @return publish this topic of type
+ * @brief This is the driver for the Seek camera.
+ * The class operates on a timer, collecting images from the 
+ * camera and pumping them into messages for the rest of the
+ * system.
+ * 
+ * On init: Camera is found and opened
+ * 
+ * On timer loop: Camera image is captured, pumped into a message
+ * 
+ * @see Seek API documentation for explanations of the function
+ * calls  
+ * 
  */
 class SeekDriver
 {
 public:
   SeekDriver();
+
+  /**
+   * @brief Destroy the Seek Driver object. Seek camera
+   * needs to be closed manually, or else it may have issues
+   * on re-boot
+   * 
+   */
   ~SeekDriver();
 
 private:
   /////////////////////// State variables ///////////////////////
 
+  sw* camera_;
+
+  //Container for temperature values from camera
+  std::vector<float> thermographyData_;
+
+  //Container for semi-processed sensor readings from the camera.
+  //NOTE: Although this sounds like the 'typical' IR image return, it
+  //is NOT. Its the semi-processed direct sensor reading with no gain
+  //control. If you try and view the image immediately it will look weird
+	std::vector<unsigned short> filteredData_;
+
+  //Same as above but with an extra row appended to the bottom
+  //of the data containing camera telemetry data. See Seek API documentation
+	std::vector<unsigned short> filteredWithTelemetryData_;
+
+  //This is the gain controlled IR camera returns, what you
+  //typically think of when you see a IR image
+	std::vector<unsigned int> displayData_;
+
+  int frameCount_ = 0;
+  
+  /**
+   * Display Data returned from Seek is noted as ARGB888
+   * in the Seek API. The encoding for CV_8UC4 (8 bit, 4 channel)
+   * is BGRA32. While these may seem different, the ARGB888 is
+   * little endian notation, thus the channels require no mixing
+   * and can be put directly in a cv matrix of type CV_8UC4
+   * 
+   * In this implementation, the cv matrix is pointed directly
+   * to the image buffer, so the matrix itself doesn't need to
+   * be updated every run loop
+   */
+  cv::Mat displayImageMatrix_;
 
   /////////////////////// calculation variables ///////////////////////
 
  
 
-  // put your most important function and other helper functions here
   /**
    * @brief main loop over the ros timer that does this, process what, and does whaaat
    *
@@ -52,10 +98,24 @@ private:
    */
   void run();
 
+  /**
+   * @brief Used to process the Seek return codes, which are returned
+   * by most calls to the seek API. Checks if an error has occured,
+   * and will print to ROS_ERROR. If there is nothing abnormal
+   * with the return code, then this call is silent
+   * 
+   * @param returnError Seek API Return Error Code
+   * @param context Context that the error occurred in e.g. 
+   * finding camera, closing camera
+   */
+  void processReturnCode(sw_retcode returnError, std::string context);
+
   // ROS and node related variables
   ros::NodeHandle nh_, private_nh_;
   ros::Timer timer_;
   double timerFreq_;
+
+  image_transport::Publisher displayImagePub_;
 
   // Boiler plate ROS functions
   void initRos();
--- src/seek_driver.cpp
+++ src/seek_driver.cpp
@@ -1,28 +1,91 @@
-// Author(s): Kelvin Kang (kelvinkang@cmu.edu)
+// Author(s): Daniel Bronstein (dbronste@andrew.cmu.edu)
 // This file is subject to the terms and conditions defined in the file 'LICENSE',
 // which is part of this source code package
 
-// include in the cpp node file should only be the .h file
-// any additional include should go to .h file
 #include "seek_driver.h"
 
-// convention is to put Constructor at the very top
-SeekDriver::SeekDriver() : private_nh_("~")
+SeekDriver::SeekDriver() : private_nh_("~"), camera_(NULL)
 {
-  // put boiler plate ROS stuff in this function
   initRos();
 
-  // initialise variables if you need to and add explanation to magic numbers
+  sw_retcode mostRecentReturn;//hold return codes to process
+
+  int numCamsToFind = 1;
+  sw* cameraList[numCamsToFind];
+  int numCamsFound = 0;
+  mostRecentReturn = Seekware_Find(cameraList, numCamsToFind, &numCamsFound);
+  processReturnCode(mostRecentReturn, "Finding Camera");
+
+  if (numCamsFound == 0 || !camera_) {
+		ROS_ERROR("Seek camera not found");//TODO: Replace with custom message
+    return;
+	}
+
+  //grab first (only) camera from camera list
+  camera_ = cameraList[0];
+
+  //Stop camera first, in case failed close last time
+	mostRecentReturn = Seekware_Stop(camera_);
+	processReturnCode(mostRecentReturn, "Stopping Camera pre-Open");
+
+  //Alright now open it
+	mostRecentReturn = Seekware_Open(camera_);
+	processReturnCode(mostRecentReturn, "Opening Camera");
+
+  int totalNumPixels = camera_->frame_rows * camera_->frame_cols;
+
+  //initialize image buffers
+  thermographyData_ = std::vector<float>(totalNumPixels);
+  filteredData_ = std::vector<unsigned short>(totalNumPixels);
+  filteredWithTelemetryData_ = std::vector<unsigned short>(totalNumPixels + camera_->frame_cols);
+  displayData_ =  std::vector<unsigned int>(totalNumPixels);
+
+  //reset and start timer TODO: I believe this sets it to zero, need to
+  //adjust returns to get in line with the wall clock
+  int enable = 1;
+	Seekware_SetSettingEx(camera_, SETTING_ENABLE_TIMESTAMP, &enable, sizeof(enable));
+	Seekware_SetSettingEx(camera_, SETTING_RESET_TIMESTAMP, &enable, sizeof(enable));
+
+  //using ptr version requires no updating in run loop
+  displayImageMatrix_ = cv::Mat(camera_->frame_rows, 
+    camera_->frame_cols, CV_8UC4, displayData_.data());
+
+  frameCount_ = 0;
 
 }
 
-// convention is to put the most important function right after the constructor
 void SeekDriver::run()
 {
-  // do really cool and important stuff here
-  ROS_INFO("In Run");
-  // publish the result of your calculation / algo
-  // vehicleStatsPub_.publish(vehicleStatsMsg_);
+  //if we have a camera (remember, its a pointer)
+  if (camera_){
+    //Grab image, process return code
+    sw_retcode returnErrorCode = Seekware_GetImageEx(camera_, 
+                    filteredWithTelemetryData_.data(), 
+                    thermographyData_.data(), 
+                    displayData_.data());
+    processReturnCode(returnErrorCode, "Getting Image from Camera");
+
+    std_msgs::Header head;
+    head.seq = frameCount_;
+    head.stamp = ros::Time::now();
+    sensor_msgs::ImagePtr msg = cv_bridge::CvImage(head, 
+      "bgr8", displayImageMatrix_).toImageMsg();
+  
+    displayImagePub_.publish(msg);
+    ++frameCount_;
+  }
+}
+
+void SeekDriver::processReturnCode(sw_retcode returnError, std::string context)
+{
+  if (returnError != SW_RETCODE_NONE) {
+      std::ostringstream errStringStream;
+      errStringStream << "Seek Failed in context: " << context
+      << " \n\tReturn Code: " <<  returnError;
+
+      //TODO: Replace with custom message
+      ROS_ERROR("%s", errStringStream.str().c_str());
+    }
 }
 
 // put boiler plate ros function at the bottom
@@ -34,15 +97,16 @@ void SeekDriver::timerCallback(const ros::TimerEvent& e)
 
 void SeekDriver::initRos()
 {
-  // param syntax is: param name in launch, variable name in source code, and default value
-  // private_nh_.param<int>("myVar1_", myVar1_, 1);
-  // private_nh_.param<double>("myVar2_", myVar2_, 5.0);
-  private_nh_.param<double>("timerFreq_", timerFreq_, 20.0);  // Hz
+  //default doesn't need to be over 9.0 since that is camera frame rate
+  //limit
+  private_nh_.param<double>("timerFreq_", timerFreq_, 9.0);  // Hz
 
   // set subscriber
   // odomSub_ = nh_.subscribe("/odom", 1, &SeekDriver::odomCallback, this);
 
   // set publisher
+  image_transport::ImageTransport it(nh_);
+  displayImagePub_ = it.advertise("seek_camera/displayImage", 10);
   // vehicleStatsPub_ = nh_.advertise<std_msgs::String>("/vehicle_status", 10);
 
   // set timers
@@ -50,10 +114,11 @@ void SeekDriver::initRos()
 }
 
 SeekDriver::~SeekDriver() {
-  
+  if (camera_ != NULL) {
+		Seekware_Close(camera_);
+	}
 }
 
-// keep the main function as clean as possible
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "seek_driver");
@@ -62,9 +127,3 @@ int main(int argc, char** argv)
     ros::spinOnce();
   return 0;
 }
-/*
- * My Questions:
- *  - Parameters in the launch files associated with a node are public?
- *    And the default initialized done by private_nh is private?
- *  - Use of timer vs sleep in main function
- */
