0d38dc96f163d65eb30b69fdf6651edc6c83cd2c
Dan Bronstein <danbronzy@gmail.com>
2021-04-07 11:51:18 -0400

    Moved message and service to dragoon_messages

--- include/seek_driver.h
+++ include/seek_driver.h
@@ -18,8 +18,8 @@
 #include <cv_bridge/cv_bridge.h>
 
 //my srvs and msgs
-#include "seek_driver/restartCamera.h"
-#include "seek_driver/telemetryData.h"
+#include "dragoon_messages/restartCamera.h"
+#include "dragoon_messages/telemetryData.h"
 
 // Include other header files
 #include <seekware.h>
@@ -138,8 +138,8 @@ private:
    * @return true if camera is found and reinitilized without errors
    * @return false otherwise
    */
-  bool restartCameraServiceCB(seek_driver::restartCameraRequest& req,
-                              seek_driver::restartCameraResponse& resp);
+  bool restartCameraServiceCB(dragoon_messages::restartCameraRequest& req,
+                              dragoon_messages::restartCameraResponse& resp);
 
   /**
    * @brief Used to process the Seek return codes, which are returned
